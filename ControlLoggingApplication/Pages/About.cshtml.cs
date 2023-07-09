732038b7e1a6788c0e2f517c7c54ec4bf360f86c
Karen Payne <paynekaren@comcast.net>
2023-07-09 12:38:05 -0700

    refinements

--- ControlLoggingApplication/Pages/About.cshtml.cs
+++ ControlLoggingApplication/Pages/About.cshtml.cs
@@ -12,16 +12,20 @@ namespace ControlLoggingApplication.Pages
         public string Title { get; set; }
         private readonly ApplicationFeatures _features;
 
+        public bool LoggingEnabled { get; set; }
+
         public AboutModel(IOptionsSnapshot<ApplicationFeatures> features)
         {
             _features = features.Get(ApplicationFeatures.About);
             Title = _features.Title;
+            LoggingEnabled = _features.EnableLogging;
         }
         public void OnGet()
         {
             if (_features.EnableLogging)
             {
                 Log.Information("OnGet for About Page");
+                
             }
         }
     }
