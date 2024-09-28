2ed1fd6c8c905ececca85dacf358d47fedcb617f
Karen Payne <paynekaren@comcast.net>
2024-09-28 04:44:03 -0700

    Refactor SetupServices and update project dependencies
    Refactored the `SetupServices` class in `SetupServices.cs` to remove the dependency on `EntityConfiguration` and its constructor parameter. Added XML documentation comments to the class constructor and methods for better readability and maintainability.
    
    Updated `ExcelConnectionApplication.csproj` to:
    - Upgrade `ConsoleConfigurationLibrary` to version `1.0.0.4`
    - Upgrade `ExcelMapper` to version `5.2.592`
    - Upgrade `Microsoft.Data.SqlClient` to version `5.2.2`
    
    Enhanced `readme.md` with a brief description of the application, details about `appsettings.json`, dependency injection, project purpose, motivation, and a reference to a Stack Overflow question.

--- ExcelConnectionApplication/Classes/Configuration/SetupServices.cs
+++ ExcelConnectionApplication/Classes/Configuration/SetupServices.cs
@@ -5,26 +5,37 @@ namespace ExcelConnectionApplication.Classes.Configuration;
 internal class SetupServices
 {
     private readonly ExcelConnectionStrings _excelOptions;
-    private readonly EntityConfiguration _settings;
     private readonly ConnectionStrings _options;
 
-    public SetupServices(
-        IOptions<ConnectionStrings> options,
-        IOptions<ExcelConnectionStrings> excelOptions,
-        IOptions<EntityConfiguration> settings)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SetupServices"/> class.
+    /// </summary>
+    /// <param name="options">The application database connection strings.</param>
+    /// <param name="excelOptions">The Excel connection strings.</param>
+    public SetupServices(IOptions<ConnectionStrings> options, IOptions<ExcelConnectionStrings> excelOptions)
     {
         _excelOptions = excelOptions.Value;
         _options = options.Value;
-        _settings = settings.Value;
     }
     /// <summary>
-    /// Read connection strings from appsettings
+    /// Reads connection strings from the application settings for the database.
     /// </summary>
+    /// <remarks>
+    /// This method retrieves the main connection string for the database from the application settings
+    /// and assigns it to the singleton instance of <see cref="DataConnections"/>.
+    /// </remarks>
     public void GetConnectionStrings()
     {
         DataConnections.Instance.MainConnection = _options.MainConnection;
     }
 
+    /// <summary>
+    /// Reads connection strings from appsettings for Excel files.
+    /// </summary>
+    /// <remarks>
+    /// This method retrieves the main connection string for Excel files from the application settings
+    /// and assigns it to the singleton instance of <see cref="ExcelConnections"/>.
+    /// </remarks>
     public void GetExcelConnectionStrings()
     {
         ExcelConnections.Instance.MainConnection = _excelOptions.MainConnection;
--- ExcelConnectionApplication/readme.md
+++ ExcelConnectionApplication/readme.md
@@ -1,2 +1,29 @@
 # About
 
+This application that reads an excel file and displays the content in a DataGridView.
+
+- appsettings.json contains an Excel file name used by [ExcelMapper](https://www.nuget.org/packages/ExcelMapper/5.2.590?_src=template) to read the file.
+- appsettings.json contains a database connection string not used, here to show that we can do this same as with Excel.
+- See Program.cs which sets up the configuration using dependency injection.
+- Classes.Configuration has code to read settings from appsettings.json into singleton objects.
+- Models.Configuration has the objects that hold the settings.
+
+## Project type
+
+Although this is done in Windows Forms project the code to work with settings in appsettings.json is the same for any .NET project type.
+
+## Depenency Injection
+
+
+Some developer may think this is overkill for a small application like this, but it is a good practice to use dependency injection. It makes the code more testable and easier to maintain.
+
+:+1:
+If all developer projects use what is presented the configuration code can be placed in a class project then create a local NuGet package to use in all projects that require connections.
+
+## Notes
+
+:heavy_check_mark: 
+The reason for this project was after seeing a [question](https://stackoverflow.com/questions/79029134/the-microsoft-access-database-engine-could-not-find-the-object-in-c-sharp-on-the) on how to read an Excel file. The question was not answered, so I decided to create this project to show how it can be done.
+
+:x: 
+Did not reply to the question as I doubt they would have understood the answer. The question was not clear, and the person asking the question did not seem to have much experience with programming.
