3f8f19afd77fc0e937a5eb0e11e074c41e9140cb
Karen Payne <paynekaren@comcast.net>
2025-03-01 17:14:55 -0800

    Refactor Azure settings classes
    Removed `TenantNameAzureSettings` class and its properties.
    Added `TenantAzureSettings` class with the same properties
    in a new file, including the necessary namespace declaration.
    
    by Karen

--- IOptionsMonitorAzureSettingsApp/Models/TenantAzureSettings.cs
+++ IOptionsMonitorAzureSettingsApp/Models/TenantAzureSettings.cs
@@ -1,6 +1,6 @@
 ﻿namespace IOptionsMonitorAzureSettingsApp.Models;
 
-public class TenantNameAzureSettings
+public class TenantAzureSettings
 {
     public string ConnectionString { get; set; } = string.Empty;
     public string TenantId { get; set; } = string.Empty;
