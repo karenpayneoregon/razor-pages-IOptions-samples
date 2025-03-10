7d9469872e5966fd3123892280004782b918f639
Karen Payne <paynekaren@comcast.net>
2025-03-10 06:31:55 -0700

    Improve documentation and refactor configuration access
    Updated the `ApplicationConfiguration.cs` file to enhance the documentation for the `ConfigureServices` method, clarifying its purpose regarding dependency injection and service registration. Refactored the configuration access to use `Config.JsonRoot()`.
    
    Modified the `ExcelConnectionApplication.csproj` file to change the `Using` directive namespace from `ConsoleConfigurationLibrary.Classes` to `ConsoleConfigurationLibrary.Classes.Configuration`, reflecting a restructuring of the project's namespaces.
    
    by Karen

--- ExcelConnectionApplication/Classes/Configuration/ApplicationConfiguration.cs
+++ ExcelConnectionApplication/Classes/Configuration/ApplicationConfiguration.cs
@@ -6,20 +6,24 @@ namespace ExcelConnectionApplication.Classes.Configuration;
 internal class ApplicationConfiguration
 {
     /// <summary>
-    /// Sets up the services for connection string and should mock data be used
+    /// Configures and returns a new instance of <see cref="ServiceCollection"/> 
+    /// with services required for handling connection strings and setup operations.
     /// </summary>
-    /// <returns>ServiceCollection</returns>
+    /// <remarks>
+    /// This method sets up dependency injection for connection strings and Excel connection strings
+    /// by reading configurations from the application settings. It also registers 
+    /// <see cref="SetupServices"/> as a transient service.
+    /// </remarks>
+    /// <returns>
+    /// A configured instance of <see cref="ServiceCollection"/>.
+    /// </returns>
     public static ServiceCollection ConfigureServices()
     {
         static void ConfigureService(IServiceCollection services)
         {
 
-            services.Configure<ConnectionStrings>(Config.Configuration.JsonRoot()
-                .GetSection(nameof(ConnectionStrings)));
-
-            services.Configure<ExcelConnectionStrings>(Config.Configuration.JsonRoot()
-                .GetSection(nameof(ExcelConnectionStrings)));
-
+            services.Configure<ConnectionStrings>(Config.JsonRoot().GetSection(nameof(ConnectionStrings)));
+            services.Configure<ExcelConnectionStrings>(Config.JsonRoot().GetSection(nameof(ExcelConnectionStrings)));
             services.AddTransient<SetupServices>();
         }
 
