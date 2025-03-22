11b1792e1e0e28d8e4e2e473b85d320f7a5f9486
Karen Payne <paynekaren@comcast.net>
2025-03-22 08:26:40 -0700

    :small_orange_diamond: docs

--- KeyPerFileProviderSample/Pages/Index.cshtml.cs
+++ KeyPerFileProviderSample/Pages/Index.cshtml.cs
@@ -6,6 +6,18 @@ using Microsoft.Extensions.Options;
 
 namespace KeyPerFileProviderSample.Pages;
 
+/// <summary>
+/// Represents the model for the Index page, providing data for display and handling page logic.
+/// </summary>
+/// <remarks>
+/// This class retrieves configuration data for the HelpDesk and Connections settings using dependency injection.
+/// </remarks>
+/// <param name="helpDeskOptions">
+/// An instance of <see cref="IOptions{TOptions}"/> for accessing HelpDesk configuration values.
+/// </param>
+/// <param name="connections">
+/// An instance of <see cref="IOptions{TOptions}"/> for accessing Connections configuration values.
+/// </param>
 public class IndexModel(IOptions<HelpDesk> helpDeskOptions, IOptions<Connections> connections) : PageModel
 {
     private readonly HelpDesk _helpDesk = helpDeskOptions.Value;
